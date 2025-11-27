e519815ecdf2a3dd64135ad5af356ad42e73d3bf
Jacob Kulcak <144879268+JacobKulcak@users.noreply.github.com>
2025-11-26 23:09:04 -0500

    refactor how cut filters are created"

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -231,7 +231,7 @@ void SimpleEQAudioProcessor::updatePeakFilter(const ChainSettings& chainSettings
 	updateCoefficients(rightChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
 }
 
-void /*SimpleEQAudioProcessor::*/updateCoefficients(Coefficients& old, const Coefficients& replacements)
+void updateCoefficients(Coefficients& old, const Coefficients& replacements)
 {
     *old = *replacements;
 }
